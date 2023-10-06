6bbb002b54cd92e329e456c96885ae643e13ccc9
aeiaieo <alexislaineboldt@gmail.com>
2023-10-06 09:08:36 -0700

    Project start
    React with Vite integrated. Header designed.

---
+++ meme-generator/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import React from 'react'
+// import ReactDOM from 'react-dom/client'
+import Header from './components/Header'
+import './App.css'
+
+export default function App() {
+  return(
+    <div>
+      <Header />
+    </div>
+    
+  )
+  
+}
---
+++ meme-generator/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+export default function Header(){
+    return(
+        <div className='header'>
+            <img src='vite.svg'></img> <h1>Meme Generator</h1>
+            <p>React Course - Project 3</p>
+        </div>
+    )
+}
