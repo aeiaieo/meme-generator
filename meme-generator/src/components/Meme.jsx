726e00985501cbf284dc95799609aaba6e519be6
aeiaieo <alexislaineboldt@gmail.com>
2023-10-08 13:27:22 -0700

    Image generated
    Memes data integrated and programmed to appear and change on click.

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import memesData from '../memesData.jsx'
 
 export default function Meme(){
+
+const [memeImage, setMemeImage] = React.useState('')
+
+    function getMeme(){
+        const memesArray = memesData.data.memes
+        const randomNum = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNum].url)
+    }
     return(
-        <div className='meme'>
-            <form>
-                <input></input>
-                <input></input>
-                <button>New meme</button>
-            </form>
-        </div>
+        <main>
+            <div className='meme'>
+                <input placeholder='top text'></input>
+                <input placeholder='bottom text'></input>
+                <button onClick={getMeme}>can i haz meme plz</button>
+            </div>
+            <img src={memeImage} className='meme--image'/>
+        </main>
     )
 }
